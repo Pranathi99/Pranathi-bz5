6a1077b2798024638f8fc9776c9f966aea462488
pranathi99 <pranathi.ganti@gmail.com>
2020-01-08 11:29:47 +0530

    Changes to angular project with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+import{HomeComponent} from './home/home.component';
+import{UserComponent} from './user/user.component';
+import{CountryComponent} from './country/country.component';
+import{ColorComponent} from './color/color.component';
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'colors',component:ColorComponent},
+  {path:'countries',component:CountryComponent},
+  {path:'users',component:UserComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
